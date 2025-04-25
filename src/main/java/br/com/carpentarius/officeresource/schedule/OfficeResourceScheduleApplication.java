d8835591017203a696befeb321fac274e4f5c1d6
danilo-ambrosio <danilo.ambrosio@gmail.com>
2025-04-25 18:40:31 -0300

    Project Structure, Resource Type entity and registration operation at domain level

---
+++ src/main/java/br/com/carpentarius/officeresource/schedule/OfficeResourceScheduleApplication.java
@@ -0,0 +1,13 @@
+package br.com.carpentarius.officeresource.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeResourceScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeResourceScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/carpentarius/officeresource/schedule/OfficeResourceScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carpentarius.officeresource.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeResourceScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
